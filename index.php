7818eba40a716ce9df963e43677f5f01d7109911
manuel <manuelguma25@gmail.com>
2021-09-23 17:53:29 -0500

    cabecera

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <!--CABECERA-->
+
+    <header id="header">
+        <div id="logo">
+            <img src="assets/img/camiseta.png" alt="camiseta logo ">
+            <a href="index.php">
+            tienda de camisetas
+        </a>
+        </div>
+    </header>
+    <!--MENU-->
+
+    <!--BARRA LATERAL-->
+
+    <!--CONTENIDO CENTRAL-->
+
+
+    <!--PIE DE PAGINA-->
+</body>
+
+</html>
