9e08df5a81e50a7d934fbe6d475807dc7679dc25
manuel <manuelguma25@gmail.com>
2021-09-23 18:08:15 -0500

    barra lateral html

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -22,7 +24,46 @@
     </header>
     <!--MENU-->
 
-    <!--BARRA LATERAL-->
+    <nav id="menu">
+        <ul>
+            <li>
+                <a href="#">Inicio</a>
+            </li>
+            <li>
+                <a href="#">categoria</a>
+            </li>
+            <li>
+                <a href="#">categoria</a>
+            </li>
+            <li>
+                <a href="#">categoria</a>
+            </li>
+            <li>
+                <a href="#">categoria 1</a>
+            </li>
+        </ul>
+    </nav>
+    <div id="content">
+        <!--BARRA LATERAL-->
+        <aside id="lateral">
+            <div id="login" class="block_aside">
+                <form action="" method="POST">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+
+                    <label for="password">Password</label>
+                    <input type="password" name="password">
+                    <input type="submit" value="enviar">
+                    <br>
+                    <a href="">mis pedidos</a>
+                    <a href="">gestionar pedidos</a>
+                    <a href=""> gestionar categorias</a>
+
+                </form>
+            </div>
+        </aside>
+    </div>
+
 
     <!--CONTENIDO CENTRAL-->
 
