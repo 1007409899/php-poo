ac8c51af4d1e407151a28530017e3fc38a2347c5
manuel <manuelguma25@gmail.com>
2021-09-23 18:20:17 -0500

    cotenido central  y pie de pagina en html

--- index.php
+++ index.php
@@ -1,74 +1,104 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+      crossorigin="anonymous"
+    />
+  </head>
 
-</head>
-
-<body>
+  <body>
     <!--CABECERA-->
 
     <header id="header">
-        <div id="logo">
-            <img src="assets/img/camiseta.png" alt="camiseta logo ">
-            <a href="index.php">
-            tienda de camisetas
-        </a>
-        </div>
+      <div id="logo">
+        <img src="assets/img/camiseta.png" alt="camiseta logo " />
+        <a href="index.php"> tienda de camisetas </a>
+      </div>
     </header>
     <!--MENU-->
 
     <nav id="menu">
-        <ul>
-            <li>
-                <a href="#">Inicio</a>
-            </li>
-            <li>
-                <a href="#">categoria</a>
-            </li>
-            <li>
-                <a href="#">categoria</a>
-            </li>
-            <li>
-                <a href="#">categoria</a>
-            </li>
-            <li>
-                <a href="#">categoria 1</a>
-            </li>
-        </ul>
+      <ul>
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">categoria</a>
+        </li>
+        <li>
+          <a href="#">categoria</a>
+        </li>
+        <li>
+          <a href="#">categoria</a>
+        </li>
+        <li>
+          <a href="#">categoria 1</a>
+        </li>
+      </ul>
     </nav>
     <div id="content">
-        <!--BARRA LATERAL-->
-        <aside id="lateral">
-            <div id="login" class="block_aside">
-                <form action="" method="POST">
-                    <label for="email">Email</label>
-                    <input type="email" name="email">
+      <!--BARRA LATERAL-->
+      <aside id="lateral">
+        <div id="login" class="block_aside">
+          <form action="" method="POST">
+            <label for="email">Email</label>
+            <input type="email" name="email" />
 
-                    <label for="password">Password</label>
-                    <input type="password" name="password">
-                    <input type="submit" value="enviar">
-                    <br>
-                    <a href="">mis pedidos</a>
-                    <a href="">gestionar pedidos</a>
-                    <a href=""> gestionar categorias</a>
-
-                </form>
-            </div>
-        </aside>
+            <label for="password">Password</label>
+            <input type="password" name="password" />
+            <input type="submit" value="enviar" />
+            <br />
+            <a href="">mis pedidos</a>
+            <a href="">gestionar pedidos</a>
+            <a href=""> gestionar categorias</a>
+          </form>
+        </div>
+      </aside>
     </div>
 
-
     <!--CONTENIDO CENTRAL-->
-
+    <div id="central">
+      <div class="product">
+        <img src="assets/img/camiseta.png" alt="" />
+        <h2>Camiseta azul ancha</h2>
+        <p>30 euros</p>
+        <a href="">Comprar</a>
+      </div>
+      <div class="product">
+        <img src="assets/img/camiseta.png" alt="" />
+        <h2>Camiseta azul ancha</h2>
+        <p>30 euros</p>
+        <a href="">Comprar</a>
+      </div>
+      <div class="product">
+        <img src="assets/img/camiseta.png" alt="" />
+        <h2>Camiseta azul ancha</h2>
+        <p>30 euros</p>
+        <a href="">Comprar</a>
+      </div>
+      <div class="product">
+        <img src="assets/img/camiseta.png" alt="" />
+        <h2>Camiseta azul ancha</h2>
+        <p>30 euros</p>
+        <a href="">Comprar</a>
+      </div>
+    </div>
 
     <!--PIE DE PAGINA-->
-</body>
 
+    <footer id="footer">
+      <p>
+        Desarrollado Por manuel Guzman &copy;
+        <?=date('Y')?>
+      </p>
+    </footer>
+  </body>
 </html>
